d98262a1205a51d2b9b3e0a837274abc4de45263
nripoll <nikoripollk@gmail.com>
2017-01-09 15:46:44 -0300

    Finished first version of the code
    Testing is required, the rest of the code shall be parsed into python to check for improvements in calculation/coding times.

--- tdma.py
+++ tdma.py
@@ -1,5 +1,25 @@
+import numpy as np
+import njit
+
 #Tri Diagonal Matrix Algorithm
+@njit
+def tdma(A, B, C, D):
+    '''
+    TDMA solver, a b c d can be NumPy array type or Python list type.
+    refer to http://en.wikipedia.org/wiki/Tridiagonal_matrix_algorithm
+    and to http://www.cfd-online.com/Wiki/Tridiagonal_matrix_algorithm_-_TDMA_(Thomas_algorithm)
+    '''
+    nf = len(D) # number of equations
+    ac, bc, cc, dc = map(np.array, (A, B, C, D)) # copy arrays
+    for it in xrange(1, nf):
+        mc = ac[it-1]/bc[it-1]
+        bc[it] = bc[it] - mc*cc[it-1] 
+        dc[it] = dc[it] - mc*dc[it-1]
+        	    
+    xc = bc
+    xc[-1] = dc[-1]/bc[-1]
 
-def tdma (A, B, C, D, N)
+    for il in xrange(nf-2, -1, -1):
+        xc[il] = (dc[il]-cc[il]*xc[il+1])/bc[il]
 
-# 
+    return xc
